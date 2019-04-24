57ed793f8ed691a7510c444355a936eae17f0756
vagrant <vagrant@homestead>
2019-04-24 08:21:54 +0000

    分类话题列表

--- routes/web.php
+++ routes/web.php
@@ -40,5 +40,8 @@ Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit'
 // Route::get('/users/{user}', 'UsersController@show')->name('users.show');
 // Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
 // Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
-
+//话题相关路由
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+
+//分类相关路由
+Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
