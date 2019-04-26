3c7a551719904cb47c7088f123afc76f598a1578
vagrant <vagrant@homestead>
2019-04-26 01:52:00 +0000

    回复数据

--- routes/web.php
+++ routes/web.php
@@ -49,3 +49,5 @@ Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 
 //图片上传路由
 Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
+
+Route::resource('replies', 'RepliesController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
