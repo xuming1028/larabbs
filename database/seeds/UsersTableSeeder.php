77666e1f71dd5f72044f52265c2519f80b221831
vagrant <vagrant@homestead>
2019-04-26 02:49:00 +0000

    回复列表

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -45,7 +45,7 @@ class UsersTableSeeder extends Seeder
         // 单独处理第一个用户的数据
         $user = User::find(1);
         $user->name = 'ShiHun';
-        $user->email = '4617703361@qq.com';
+        $user->email = '461770336@qq.com';
         $user->avatar = 'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png';
         $user->save();
     }
