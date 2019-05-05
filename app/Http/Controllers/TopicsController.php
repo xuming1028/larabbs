b76a60c8a4d0bef1994487315c1abcb08e8298fb
vagrant <vagrant@homestead>
2019-05-05 06:44:21 +0000

    侧边栏连接

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -10,6 +10,7 @@ use App\Models\Category;
 use Auth;
 use App\Handlers\ImageUploadHandler;
 use App\Models\User;
+use App\Models\Link;
 
 class TopicsController extends Controller
 {
@@ -24,12 +25,15 @@ class TopicsController extends Controller
 	// 	return view('topics.index', compact('topics'));
 	// }
 
-    public function index(Request $request, Topic $topic , User $user)
+    public function index(Request $request, Topic $topic , User $user, Link $link)
     {
         $topics = $topic->withOrder($request->order)->paginate(20);
+        //活跃用户
         $active_users = $user->getActiveUsers();
+        //资源链接
+        $links = $link->getAllCached();
         // dd($active_users);
-        return view('topics.index', compact('topics', 'active_users'));
+        return view('topics.index', compact('topics', 'active_users','links'));
     }
 
     public function show(Request $request, Topic $topic)
